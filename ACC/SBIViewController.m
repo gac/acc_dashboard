d8a4077de2e143c608091bab6cbb130dfd0e9b65
Juan Manuel <ga_c@ymail.com>
2012-03-23 15:33:03 -0600

    - se integro el login

--- ACC/SBIViewController.m
+++ ACC/SBIViewController.m
@@ -1,17 +1,29 @@
 //
 //  SBIViewController.m
-//  ACC
+//  acc
 //
-//  Created by Juan Galicia Castillo on 3/22/12.
+//  Created by Juan Galicia Castillo on 2/23/12.
 //  Copyright (c) 2012 Scotiabank. All rights reserved.
 //
 
 #import "SBIViewController.h"
+
 #import "SBIAppDelegate.h"
 #import "RootViewController.h"
 #import "DetailViewController.h"
 
+#include <unistd.h>
+
 @implementation SBIViewController
+
+@synthesize logoImageView;
+@synthesize usernameLabel;
+@synthesize usernameField;
+@synthesize passwordLabel;
+@synthesize passwordField;
+@synthesize activityIndicatorLabel;
+@synthesize activityIndicatorView;
+@synthesize copyrightLabel;
 
 - (void)didReceiveMemoryWarning
 {
@@ -32,6 +44,10 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.usernameField = nil;
+    self.passwordField = nil;
+    self.activityIndicatorLabel = nil;
+    self.activityIndicatorView = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -60,23 +76,97 @@
     return YES;
 }
 
--(IBAction)onClick1:(id)sender {
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
     
-    SBIAppDelegate *appDelegate = (SBIAppDelegate *)[[UIApplication sharedApplication] delegate];
+    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft ||
+        toInterfaceOrientation == UIInterfaceOrientationLandscapeRight)
+    {
         
-    // Override point for customization after app launch.
-    appDelegate.splitViewController =[[UISplitViewController alloc]init];
-    appDelegate.rootViewController = [[RootViewController alloc]init];
-    appDelegate.detailViewController = [[DetailViewController alloc]init];
-	
-	UINavigationController *rootNav=[[UINavigationController alloc]initWithRootViewController:appDelegate.rootViewController];
-    UINavigationController *detailNav=[[UINavigationController alloc]initWithRootViewController:appDelegate.detailViewController];
+        logoImageView.frame = CGRectMake(309, 116, 100, 120);
+        usernameLabel.frame = CGRectMake(417, 141, 115, 21);
+        usernameField.frame = CGRectMake(540, 136, 175, 31);
+        passwordLabel.frame = CGRectMake(417, 185, 115, 21);
+        passwordField.frame = CGRectMake(540, 180, 175, 31);
+        activityIndicatorView.frame = CGRectMake(437, 355, 37, 37);
+        activityIndicatorLabel.frame = CGRectMake(482, 363, 105, 21);
+        copyrightLabel.frame = CGRectMake(112, 707, 800, 21);
+        
+    } else {
+        
+        logoImageView.frame = CGRectMake(181, 191, 100, 120);
+        usernameLabel.frame = CGRectMake(289, 216, 115, 21);
+        usernameField.frame = CGRectMake(412, 211, 175, 31);
+        passwordLabel.frame = CGRectMake(289, 260, 115, 21);
+        passwordField.frame = CGRectMake(412, 255, 175, 31);
+        activityIndicatorView.frame = CGRectMake(309, 483, 37, 37);
+        activityIndicatorLabel.frame = CGRectMake(354, 491, 105, 21);
+        copyrightLabel.frame = CGRectMake(-16, 963, 800, 21);
+        
+    }
+}
 
-	appDelegate.splitViewController.viewControllers=[NSArray arrayWithObjects:rootNav,detailNav,nil];
-	appDelegate.splitViewController.delegate=appDelegate.detailViewController;
+#pragma mark - login
 
-    appDelegate.window.rootViewController = appDelegate.splitViewController;
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    
+    if (textField == usernameField) {
+        
+        if (usernameField.text.length == 0) {
+            [usernameField becomeFirstResponder];
+        } else {
+            [passwordField becomeFirstResponder];
+        }
+        
+    } else if (textField == passwordField) {
+        
+        if (passwordField.text.length == 0) {
+            [passwordField becomeFirstResponder];
+        } else {
+            [usernameField becomeFirstResponder];
+        }
+        
+    }
+    
+    if (usernameField.text.length > 0 && passwordField.text.length > 0) {
+        
+        [usernameField setEnabled:NO];
+        [passwordField setEnabled:NO];
+        
+        [activityIndicatorView setHidden:NO];
+        [activityIndicatorLabel setHidden:NO];
+        
+        [activityIndicatorView startAnimating];        
+        [textField resignFirstResponder];
+        
+        SBIAppDelegate *appDelegate = (SBIAppDelegate *)[[UIApplication sharedApplication] delegate];
+        
+        // Override point for customization after app launch.
+        appDelegate.splitViewController =[[UISplitViewController alloc]init];
+        appDelegate.rootViewController = [[RootViewController alloc]init];
+        appDelegate.detailViewController = [[DetailViewController alloc]init];
+        
+        UINavigationController *rootNav=[[UINavigationController alloc]initWithRootViewController:appDelegate.rootViewController];
+        UINavigationController *detailNav=[[UINavigationController alloc]initWithRootViewController:appDelegate.detailViewController];
+        
+        appDelegate.splitViewController.viewControllers=[NSArray arrayWithObjects:rootNav,detailNav,nil];
+        appDelegate.splitViewController.delegate=appDelegate.detailViewController;
+        
+        appDelegate.window.rootViewController = appDelegate.splitViewController;
 
+        
+        
+        // Override point for customization after application launch.
+        //UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+        
+        
+        
+        //UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+        
+        //splitViewController.delegate = (id)navigationController.topViewController;
+        
+    }
+    
+    return YES;
 }
 
 @end
